4ae0c77c4085d9ceca59a1481ce76338dea9d2b0
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-07-20 14:48:06 -0300

    :style: Atualiza o estilo do arquivo init.sh

--- init.sh
+++ init.sh
@@ -3,28 +3,40 @@ set -e # Encerra em caso de erro
 set -u # Trata variáveis não definidas como erro
 set -o pipefail
 
-PACKAGE=$(yq -r ".variables.package_name.value" cookie.yml)
-
-# Copia o projeto original para o diretório atual
-#cp -r /workspace/AndroidStudioProjects/HelloAndroid .
-
-# Remove uma cópia anterior, se existir
-test -d HelloAndroid && rm -rf HelloAndroid
-
-# Extrai o arquivo.tar.gz.
-tar xvzf HelloAndroid.tar.gz
+GITHUB_USER=$1
+REPO_NAME=$2
+PATCH=$3
 
 # Remove uma cópia anterior, se existir
-test -d HelloAndroid.COPY && rm -rf HelloAndroid.COPY
+test -d ${REPO_NAME} && rm -rf ${REPO_NAME}
+
+USE_GIT_CLONE=false
+if $USE_GIT_CLONE; then
+    git clone "https://github.com/${GITHUB_USER}/${REPO_NAME}.git"
+
+    pushd "${REPO_NAME}" >/dev/null
+    git am "../${PATCH}"
+    popd >/dev/null
+else
+    tar xvzf "${REPO_NAME}.tar.gz"
+fi
+
+# ${REPO_NAME}.COPY
+# =================
+
+# Remove uma cópia anterior, se existir
+test -d ${REPO_NAME}.COPY && rm -rf ${REPO_NAME}.COPY
 
 # Faz uma nova cópia
-cp -r HelloAndroid HelloAndroid.COPY
+cp -r ${REPO_NAME} ${REPO_NAME}.COPY
+
+# ----------------------------------------------------------------------------
+# 1) primeira açao: cópia e executa os comandos git
+pushd ${REPO_NAME}.COPY >/dev/null
 
 # Remove a linha 'local.properties' do .gitignore
-sed '/local.properties/ d' -i HelloAndroid.COPY/.gitignore
+sed '/local.properties/ d' -i .gitignore
 
-# Entra no diretório da cópia e executa os comandos git
-pushd HelloAndroid.COPY >/dev/null
 for path in \
     ./.project \
     ./app/.project \
@@ -37,14 +49,21 @@ for path in \
     ./.google; do
     [[ -e "$path" ]] && rm -rf -- "$path" && echo "[rm] $path"
 done
+
 git init
 git add .
 git commit -m "first commit"
 git clean -dfx
+popd >/dev/null
 
-#PACKAGE=com.example.android.helloandroid
+# ----------------------------------------------------------------------------
+# 2) Segunda açao: package path -> __PACKAGE__
+PACKAGE=$(yq -r ".variables.package_name.value" cookie.yml)
+pushd ${REPO_NAME}.COPY >/dev/null
+root_dir="${PWD}"
 pattern="${PACKAGE//./\/}"
 
+# Process substitution: o loop é executado no shell atual, não em um subshell
 while IFS= read -r line; do
     echo "=$line="
     dir="${line%$pattern}" # ex: ./app/src/main/kotlin/
@@ -59,8 +78,7 @@ while IFS= read -r line; do
 
     #done < <(find . -depth -type d | grep "$pattern")
 done < <(find . -type d | grep "$pattern$")
-#                                       ^
-#                         ancora no fim: ignora ui/theme e ui
+
 tree .
 popd >/dev/null
 
