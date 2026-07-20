cfd9ba07a665bfc43592671f3650f3aca81da0fd
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-07-20 13:50:51 -0300

    :style: Atualiza o estilo do arquivo init.sh

--- init.sh
+++ init.sh
@@ -3,34 +3,49 @@ set -e # Encerra em caso de erro
 set -u # Trata variáveis não definidas como erro
 set -o pipefail
 
-PACKAGE=$(yq -r ".variables.package_name.value" cookie.yml)
+GITHUB_USER=$1
+REPO_NAME=$2
+#PATCH=$3
 
 # Remove uma cópia anterior, se existir
-test -d MyApplication && rm -rf MyApplication
+test -d ${REPO_NAME} && rm -rf ${REPO_NAME}
 
-# Copia o projeto original para o diretório atual
-#cp -r /workspace/AndroidStudioProjects/MyApplication .
+USE_GIT_CLONE=false
+if $USE_GIT_CLONE; then
+    git clone "https://github.com/${GITHUB_USER}/${REPO_NAME}.git"
 
-# Extrai o arquivo.tar.gz.
-tar xvzf MyApplication.tar.gz
+    pushd "${REPO_NAME}" >/dev/null
+    git am "../${PATCH}"
+    popd >/dev/null
+else
+    tar xvzf "${REPO_NAME}.tar.gz"
+fi
+
+# ${REPO_NAME}.COPY
+# =================
 
 # Remove uma cópia anterior, se existir
-test -d MyApplication.COPY && rm -rf MyApplication.COPY
+test -d ${REPO_NAME}.COPY && rm -rf ${REPO_NAME}.COPY
 
 # Faz uma nova cópia
-cp -r MyApplication MyApplication.COPY
+cp -r ${REPO_NAME} ${REPO_NAME}.COPY
 
+# ----------------------------------------------------------------------------
+# 1) primeira açao: cópia e executa os comandos git
+pushd ${REPO_NAME}.COPY >/dev/null
 # Remove a linha 'local.properties' do .gitignore
-sed '/local.properties/ d' -i MyApplication.COPY/.gitignore
-
-# Entra no diretório da cópia e executa os comandos git
-pushd MyApplication.COPY >/dev/null
+sed '/local.properties/ d' -i .gitignore
 rm -rf .idea
 git init
 git add .
 git commit -m "first commit"
 git clean -dfx
+popd >/dev/null
 
+# ----------------------------------------------------------------------------
+# 2) Segunda açao: package path -> __PACKAGE__
+PACKAGE=$(yq -r ".variables.package_name.value" cookie.yml)
+pushd ${REPO_NAME}.COPY >/dev/null
 root_dir="${PWD}"
 pattern="${PACKAGE//./\/}"
 
